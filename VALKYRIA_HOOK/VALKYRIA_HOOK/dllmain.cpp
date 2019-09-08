bf396df4e45803468486d06088a4301881d003d6
MAZ SHA-1 <47900658+MZSHproject@users.noreply.github.com>
2019-09-08 12:07:23 +0800

    add VALKYRIA hook

---
+++ VALKYRIA_HOOK/VALKYRIA_HOOK/dllmain.cpp
@@ -0,0 +1,158 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+//For プレイ！プレイ！プレイ！
+#include "framework.h"
+#include <windows.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "detours.h"
+#pragma comment(lib, "detours.lib")
+using namespace std;
+ofstream TXT("Text.txt");
+
+typedef HFONT(WINAPI *fnCreateFontA)(
+	int nHeight, // logical height of font height
+	int nWidth, // logical average character width
+	int nEscapement, // angle of escapement
+	int nOrientation, // base-line orientation angle
+	int fnWeight, // font weight
+	DWORD fdwItalic, // italic attribute flag
+	DWORD fdwUnderline, // underline attribute flag
+	DWORD fdwStrikeOut, // strikeout attribute flag
+	DWORD fdwCharSet, // character set identifier
+	DWORD fdwOutputPrecision, // output precision
+	DWORD fdwClipPrecision, // clipping precision
+	DWORD fdwQuality, // output quality
+	DWORD fdwPitchAndFamily, // pitch and family
+	LPCSTR lpszFace // pointer to typeface name string
+	);
+fnCreateFontA CreateFontAOLD;
+HFONT WINAPI CreateFontAEx(int nHeight, int nWidth, int nEscapement, int nOrientation, int fnWeight, DWORD fdwItalic, DWORD fdwUnderline, DWORD fdwStrikeOut, DWORD fdwCharSet, DWORD fdwOutputPrecision, DWORD fdwClipPrecision, DWORD fdwQuality, DWORD fdwPitchAndFamily, LPCSTR lpszFace)
+{
+	fdwCharSet = 0;
+	fnWeight = FW_SEMIBOLD;
+	return CreateFontAOLD(nHeight, nWidth, nEscapement, nOrientation, fnWeight, fdwItalic, fdwUnderline, fdwStrikeOut, fdwCharSet, fdwOutputPrecision, fdwClipPrecision, fdwQuality, fdwPitchAndFamily, "黑体");
+}
+
+PVOID g_pOldCreateWindowExA = CreateWindowExA;
+typedef HWND(WINAPI *pfuncCreateWindowExA)(
+	DWORD dwExStyle,
+	LPCTSTR lpClassName,
+	LPCTSTR lpWindowName,
+	DWORD dwStyle,
+	int x,
+	int y,
+	int nWidth,
+	int nHeight,
+	HWND hWndParent,
+	HMENU hMenu,
+	HINSTANCE hInstance,
+	LPVOID lpParam);
+HWND WINAPI NewCreateWindowExA(DWORD dwExStyle, LPCTSTR lpClassName, LPCTSTR lpWindowName, DWORD dwStyle, int x, int y, int nWidth, int nHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance, LPVOID lpParam)
+{
+	const char* szWndName = "标题测试";
+
+	return ((pfuncCreateWindowExA)g_pOldCreateWindowExA)(dwExStyle, lpClassName, (LPCTSTR)szWndName, dwStyle, x, y, nWidth, nHeight, hWndParent, hMenu, hInstance, lpParam);
+}
+
+PVOID g_pOldCreateFontIndirectA = CreateFontIndirectA;
+typedef HFONT(WINAPI *PfuncCreateFontIndirectA)(LOGFONTA *lplf);
+HFONT WINAPI NewCreateFontIndirectA(LOGFONTA *lplf)
+{
+	lplf->lfCharSet = GB2312_CHARSET;
+	strcpy(lplf->lfFaceName, "黑体");
+
+	return ((PfuncCreateFontIndirectA)g_pOldCreateFontIndirectA)(lplf);
+}
+
+
+LPWSTR ctowJIS(char* str)
+{
+	DWORD dwMinSize;
+	dwMinSize = MultiByteToWideChar(932, 0, str, -1, NULL, 0); //计算长度
+	LPWSTR out = new wchar_t[dwMinSize];
+	MultiByteToWideChar(932, 0, str, -1, out, dwMinSize);//转换
+	return out;
+}
+
+char* wtocGBK(LPCTSTR str)
+{
+	DWORD dwMinSize;
+	dwMinSize = WideCharToMultiByte(936, NULL, str, -1, NULL, 0, NULL, FALSE); //计算长度
+	char *out = new char[dwMinSize];
+	WideCharToMultiByte(936, NULL, str, -1, out, dwMinSize, NULL, FALSE);//转换
+	return out;
+}
+
+static void make_console() {
+	AllocConsole();
+	freopen("CONOUT$", "w", stdout);
+	freopen("CONIN$", "r", stdin);
+	cout << "Console\n" << endl;
+}
+
+signed int(*OldStr)(int a1, void* a2, int a3, int a4, int a5);
+signed int __cdecl MyStr(int a1, void* a2, int a3, int a4, int a5)
+{
+	_asm
+	{
+		pushad
+	}
+	char* ret;
+	int index;
+	int rep;
+	ret = (char*)a1;
+	index = *(int*)ret;
+	char* Pstr = ret + sizeof(int)-4;
+	cout <<  index << "|" << wtocGBK(ctowJIS(Pstr)) << endl;
+	TXT <<  index << "|" << wtocGBK(ctowJIS(Pstr)) << endl;
+	//wchar_t* tmpStr = ctowJIS(Pstr);
+	//if (wcscmp(tmpStr,L"/sこの作品は現実のいかなる性的暴力・虐待等の/r犯罪行為を肯定および助長するものではありません。")==0)
+	//{
+	//	strcpy(Pstr, "中文测试。");
+	//}
+	_asm
+	{
+		popad
+	}
+	return OldStr(a1, a2, a3, a4, a5);
+}
+void Hook()
+{
+	DWORD BaseAddr = (DWORD)GetModuleHandle(NULL);
+	cout << "BaseAddress:0x" <<hex<< BaseAddr << endl;
+	TXT << "BaseAddress:0x" <<hex<< BaseAddr << endl;
+	*(DWORD*)&OldStr = BaseAddr + 0x7470;
+	cout << "HookAddress:0x" << hex << (BaseAddr + 0x7470) <<"\n"<< endl;
+	TXT << "HookAddress:0x" << hex << (BaseAddr + 0x7470) << endl;
+	CreateFontAOLD = (fnCreateFontA)GetProcAddress(GetModuleHandle(L"gdi32.dll"), "CreateFontA");
+	DetourTransactionBegin();
+	DetourAttach((void**)&OldStr, MyStr);
+	DetourAttach((void**)&CreateFontAOLD, CreateFontAEx);
+	DetourAttach(&g_pOldCreateFontIndirectA, NewCreateFontIndirectA);
+	DetourAttach(&g_pOldCreateWindowExA, NewCreateWindowExA);
+	if(DetourTransactionCommit()!=NOERROR){ MessageBox(NULL, L"ERROR", L"Crescendo", MB_OK); }
+}
+
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+					 )
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		make_console();
+		Hook();
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
+extern "C" __declspec(dllexport) void dummy(void) {
+	return;
+}
